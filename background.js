7d9b221d0405221738ea3858cc1329b6f9f45f94
xylophono <49574182+xylophono@users.noreply.github.com>
2021-09-05 22:26:09 +0100

    debugging

--- background.js
+++ background.js
@@ -2,11 +2,13 @@ chrome.action.onClicked.addListener(async (tab) => {
     
     // grab current tab action title
     const title = await chrome.action.getTitle(tab.id);
+    console.log(title);
 
     // toggle design mode
     chrome.tabs.executeScript(tab.ib, {
         func: () => {
             document.designMode = title === 'Edit' ? 'on' : 'off';
+            console.log(document.designMode);
         }
     });
 
